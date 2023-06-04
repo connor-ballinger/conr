e490258869fc718f5a581fcab14a46774b96ed04
connor-ballinger <127584013+connor-ballinger@users.noreply.github.com>
2023-06-04 13:29:44 +1000

    removed code reducing font size due to error (also code styling)

--- R/knit_print.data.frame.R
+++ R/knit_print.data.frame.R
@@ -10,16 +10,22 @@
 #' @examples
 #' # redefine the default print method for objects with class "data.frame"
 #' # https://github.com/rstudio/rmarkdown-cookbook/issues/186
-knit_print.data.frame <-
-  function(df,
-           DT_opts = list(rownames = TRUE, dom = "tip", filter = "top",
-                          scrollX = TRUE, pageLength = 5,
-                          initComplete = htmlwidgets::JS(
-                            "function(settings, json) {",
-                            paste0("$(this.api().table().container()).css({'font-size': 9pt});"),
-                            "}")), ...) {
-    knitr::knit_print(DT::datatable(df, DT_opts))
-  }
+knit_print.data.frame <- function(
+
+  df,
+  DT_opts = list(
+    rownames = TRUE,
+    dom = "tip",
+    filter = "top",
+    scrollX = TRUE,
+    pageLength = 5
+  ),
+
+  ...) {
+
+  knitr::knit_print(DT::datatable(df, DT_opts))
+
+}
 
 # initComplete from https://stackoverflow.com/questions/44101055/changing-font-size-in-r-datatables-dt
 # I tried to create two functions, such that I could pass arguments to the
