94a2a3e9d6a3fe35fdc11aa6b0347108e666bb7b
connor-ballinger <127584013+connor-ballinger@users.noreply.github.com>
2023-10-15 15:54:49 +1100

    comments post fn to method

--- R/knit_print.data.frame.R
+++ R/knit_print.data.frame.R
@@ -11,11 +11,20 @@
 #'   "data.frame". For further info:
 #'   https://github.com/rstudio/rmarkdown-cookbook/issues/186. If you need to
 #'   modify the method, define knit_print.data.frame in the document, and remove
-#'   the conr:: prefix. Why DT over gt? DT: pages, choose pageLength, copy/csv
-#'   buttons gt: has most of this here
-#'   https://gt.rstudio.com/reference/opt_interactive.html, but not the download
-#'   option.
+#'   the conr:: prefix. I tried to create two functions, such that I could pass
+#'   arguments to the knitting fn (changing DT_opts) but it seems too difficult.
 #'
+#'   Why DT over gt? DT: pages, choose pageLength, copy/csv buttons. gt: has
+#'   most of this here https://gt.rstudio.com/reference/opt_interactive.html,
+#'   but not the download option.
+#'
+#'   Not sure why this changed from a function exported from conr to a method
+#'   which always automatically overwrites the default rmarkdown method, without
+#'   any need for calling function.
+#'
+#'   Currently not ideal as datatable() trims trailing zeros, leading to
+#'   inconsistent number of decimal places in a given column. DT::formatRound is
+#'   not an option as trailing zeros are never trimmed.
 #'
 #' @importFrom DT datatable
 #' @import knitr
@@ -42,17 +51,4 @@ knit_print.data.frame <- function(
 
   knitr::knit_print(DT::datatable(df, DT_opts, extensions = "Buttons"))
 
-  # numeric_cols = which(sapply(df, class) == "numeric")
-  # df |>
-  #   DT::datatable(options = DT_opts, extensions = "Buttons") |>
-  #   DT::formatRound(columns = numeric_cols, digits = 4) |>
-  #   knitr::knit_print()
-
-  # knitr::knit_print(DT::datatable(df, extensions = "Buttons", DT_opts))
-
 }
-
-# https://stackoverflow.com/questions/44101055/changing-font-size-in-r-datatables-dt
-# I tried to create two functions, such that I could pass arguments to the
-# knitting fn (changing DT_opts) but it seems too difficult.
-# Rounding and then passing to DT doesn't work as DT changes digits seen.
