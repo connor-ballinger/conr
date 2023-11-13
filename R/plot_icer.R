f1bfef5253c5d51ce4f191f37f2a1314617e4130
connor-ballinger <127584013+connor-ballinger@users.noreply.github.com>
2023-11-13 20:55:30 +1100

    dodgy icer changes

--- R/plot_icer.R
+++ R/plot_icer.R
@@ -3,7 +3,8 @@
 #' @param df A two-column dataframe with 1 column named cost, the other named effect, produced from boot.
 #' @param effect Effect column.
 #' @param cost Cost column.
-#' @param estimate A vector of length 2, the first number being effect, the second being cost
+#' @param est_effect Point estimate.
+#' @param est_cost Point estimate.
 #' @param wtp WTP per unit of effect.
 #' @param alpha Opacity of data points.
 #' @param colour Colour of point estimate.
@@ -21,19 +22,19 @@
 #' cost <- rnorm(n = 100, mean = 1000, sd = 500)
 #' effect <- rnorm(100, 3, 3)
 #' df <- data.frame(cost, effect)
-#' plot_icer(df, c(effect = 1, cost = 1000))
-#' plot_icer(df, c(effect = 1, cost = 1000), wtp = 100, alpha = 0.8)
+#' plot_icer(df, effect, cost)
+#' plot_icer(df, effect, cost, 3, 1000, wtp = 100, alpha = 0.8)
 
-plot_icer <- function(df, effect, cost, estimate, wtp, alpha = 0.5, colour = "green", size = 2) {
+plot_icer <- function(df, effect, cost, est_effect, est_cost, wtp, alpha = 0.5, colour = "green", size = 2) {
 
   # count bootstrap replications
   B = nrow(df)
 
   # portion in quadrants, clockwise
-  tr = sum(df[, 1] > 0 & df[, 2] > 0) / B
-  br = sum(df[, 1] > 0 & df[, 2] < 0) / B
-  bl = sum(df[, 1] < 0 & df[, 2] < 0) / B
-  tl = sum(df[, 1] < 0 & df[, 2] > 0) / B
+  tr = sum(pull(df, {{ effect }} ) > 0 & pull(df, {{ cost }} ) > 0) / B
+  br = sum(pull(df, {{ effect }} ) > 0 & pull(df, {{ cost }} ) < 0) / B
+  bl = sum(pull(df, {{ effect }} ) < 0 & pull(df, {{ cost }} ) < 0) / B
+  tl = sum(pull(df, {{ effect }} ) < 0 & pull(df, {{ cost }} ) > 0) / B
 
   # print as %
   quad_portions = lapply(c(tr, br, bl, tl), scales::label_percent())
@@ -64,27 +65,36 @@ plot_icer <- function(df, effect, cost, estimate, wtp, alpha = 0.5, colour = "gr
   # WTP
   if (!missing(wtp)) {
     icer_plot = icer_plot +
-      geom_abline(slope = wtp, linewidth = 0.8) +
-      geom_label(aes(x = Inf, y = wtp * max(effect, na.rm = TRUE),
-                     label = paste0("WTP = $", wtp)),
-                 hjust = "inward", vjust = 2)
+      geom_abline(slope = wtp, linewidth = 0.8)
+    # geom_label(aes(x = Inf, y = wtp * max(effect, na.rm = TRUE),
+    #                label = paste0("WTP = $", wtp)),
+    #            hjust = "inward", vjust = 2, size = 4)#atan(pi * wtp) * 180 / pi)
   }
 
   # point estimate
-  if (!missing(estimate)) {
+  if (!missing(est_effect) & !missing(est_cost)) {
     icer_plot = icer_plot +
-      geom_point(aes(x = estimate[1], y = estimate[2]),
-                 colour = colour, size = size) +
-      geom_label(aes(x = estimate[1], y = estimate[2],
-                     label = paste0(estimate[1], ", $", estimate[2])),
-                 vjust = 2)
+      geom_point(data = data.frame(effect = est_effect, cost = est_cost),
+                 aes(x = effect, y = cost),
+                 colour = colour, size = size)
+    # geom_label(data = data.frame(estimate),
+    #                  aes(x = estimate[1], y = estimate[2],
+    #                      label = paste0(estimate[1], ", $", estimate[2])))
   }
 
   # data
   icer_plot = icer_plot +
     geom_point(aes(x = .data$effect, y = .data$cost), alpha = alpha)
 
-  icer_plot
+  icer_plot +
+    coord_cartesian(
+      xlim = c(
+        min(0, pull(df, {{ effect }} )),
+        max(0, pull(df, {{ effect }} ))
+      ),
+      ylim = c(min(0, pull(df, {{ cost }} )),
+               max(0, pull(df, {{ cost }} )))
+    )
 
 }
 
