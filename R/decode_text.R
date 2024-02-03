3da5261b62c59dee9d9807ab3ea1eb03bf0a518c
Connor Ballinger <cdballinger97@gmail.com>
2024-02-03 15:39:29 +1100

    minor code change, documentation

--- R/decode_text.R
+++ R/decode_text.R
@@ -1,16 +1,21 @@
 #' Decode Text for Aesthetics
 #'
+#' @description
+#' Transform text which is used for code to something more normal-looking.
+#'
 #' @param x A character vector.
-#' @param ...
+#' @param ... Dots.
 #'
 #' @return A visually-appealing character vector.
 #' @export
 #'
 #' @examples
+#' colnames(fake_health_ec_data)
+#' colnames(fake_health_ec_data) |> decode_text()
 decode_text <- function(x, ...) {
   x = gsub("_", " ", x) # underscores to spaces
   x = tools::toTitleCase(x) # upper case
-  x = gsub("\\.", "", x) # remove periods - replace with a space?
+  x = gsub("\\.", " ", x) # remove periods - replace with a space.
 
   # columns named by years are often given x prefix, eg x2018
 
