5db5630ff7e8449c6400797288aa3e89f358362e
Connor Ballinger <cdballinger97@gmail.com>
2024-05-15 17:16:22 +1000

    dodgy

--- R/round_sensibly.R
+++ R/round_sensibly.R
@@ -15,7 +15,7 @@
 #' base::round(2.5)
 #' conr::round_sensibly(2.5)
 round_sensibly <- function(x, digits = 0) {
-  if (is(x, "numeric")) {
+  if (class(x) == "numeric") {
     posneg <- sign(x)
     z <- abs(x) * (10^digits)
     z <- z + 0.5 + sqrt(.Machine$double.eps)
