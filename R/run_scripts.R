a78d5ca72fe7d6ec3e4d2284c2473a265e11532b
Connor Ballinger <cdballinger97@gmail.com>
2024-08-29 18:10:43 +1000

    dots added

--- R/run_scripts.R
+++ R/run_scripts.R
@@ -1,6 +1,6 @@
 #' Run Scripts
 #'
-#' @description Convenience function to run all (or some) scripts in a
+#' @description Convenience function to source all (or some) scripts in a
 #'   subdirectory. The function currently assumes all scripts to be run are
 #'   named similar to `01_clean.R`, with numbers somewhere in the name to
 #'   indicate ordering.
@@ -9,6 +9,7 @@
 #'   default is in the `code` subdirectory of the open project.
 #' @param last Numeric. What is the last script to run. Default is `Inf`. If
 #'   later scripts are a work in progress, you don't want to run them.
+#' @param ... Dots to pass to \code{\link[base]{source}}.
 #'
 #' @importFrom here here
 #'
@@ -16,14 +17,12 @@
 #' @export
 #'
 #' @examples
-run_scripts <- function(directory = here::here("code"), last = Inf) {
+run_scripts <- function(directory = here::here("code"), last = Inf, ...) {
   scripts <- list.files(directory, pattern = "\\.R$")
   files_match <- regexpr("\\d+", scripts)
   numbers <- regmatches(scripts, files_match) |> as.integer()
   files_numbered <- scripts[files_match > 0]
   files <- files_numbered[which(numbers <= last)]
   files <- paste(directory, files, sep = "/")
-  lapply(files, source)
+  lapply(files, source, ...)
 }
-
-
